1575013b7a45092b94a4fff17110709b40ff9901
Elena Moise <elenamoise23@gmail.com>
2016-03-14 02:09:39 +0200

    initial commit

---
+++ ChatApplication1/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ChatApplication1.Startup))]
+
+namespace ChatApplication1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR(new HubConfiguration
+            {
+                EnableDetailedErrors = true
+            });
+        }
+    }
+}
